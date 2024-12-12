03381a7f91d7eccd1429d776a41ef7d230136694
mipxe <mariamozegova8726@gmail.com>
2024-12-12 23:36:08 +0300

    create functions for shapes

--- mozhegova.maria/T1/shapeManipulator.cpp
+++ mozhegova.maria/T1/shapeManipulator.cpp
@@ -1,9 +1,16 @@
 #include "shapeManipulator.hpp"
 #include <cstring>
 
-double * mozhegova::getNum(char * tok, size_t length)
+void mozhegova::getNum(char * tok, size_t length, double * num)
 {
-  double num[length] = {};
+  try
+  {
+    num = new double [length];
+  }
+  catch (const std::exception& e)
+  {
+    throw;
+  }
   for (size_t i = 0; tok != NULL; i++)
   {
     tok = std::strtok(NULL, " ");
@@ -12,5 +19,45 @@ double * mozhegova::getNum(char * tok, size_t length)
       num[i] = std::atof(tok);
     }
   }
-  return num;
+}
+double mozhegova::sumArea(Shape ** shapes, size_t n)
+{
+  double sum = 0;
+  for (size_t i = 0; i < n; i++)
+  {
+    sum += shapes[i]->getArea();
+  }
+  return sum;
+}
+void mozhegova::printCoorRect(Shape ** shapes, size_t n)
+{
+  for (size_t i = 0; i < n; i++)
+  {
+    rectangle_t rect = shapes[i]->getFrameRect();
+    double lx = rect.pos.x - rect.width / 2;
+    double ly = rect.pos.y - rect.height / 2;
+    double rx = rect.pos.x + rect.width / 2;
+    double ry = rect.pos.y + rect.height / 2;
+    std::cout.precision(1);
+    std::cout << " " << lx << " " << ly << " " << rx << " " << ry << "\n";
+  }
+}
+void mozhegova::scaleShapes(Shape ** shapes, size_t n, point_t p, double k)
+{
+  for (size_t i = 0; i < n; i++)
+  {
+    point_t ptr = shapes[i]->getFrameRect().pos;
+    shapes[i]->move(p);
+    point_t newptr = shapes[i]->getFrameRect().pos;
+    point_t vectorMove = {(newptr.x - ptr.x) * k, (newptr.y - ptr.y) * k};
+    shapes[i]->scale(k);
+    shapes[i]->move(-vectorMove.x, -vectorMove.y);
+  }
+}
+void mozhegova::destroy(Shape ** shapes, size_t n)
+{
+  for (size_t i = 0; i < n; i++)
+  {
+    delete shapes[i];
+  }
 }
--- mozhegova.maria/T1/shapeManipulator.hpp
+++ mozhegova.maria/T1/shapeManipulator.hpp
@@ -2,13 +2,16 @@
 #define SHAPEMANIPULATOR_HPP
 
 #include <iostream>
+#include "shape.hpp"
+#include "base-types.hpp"
 
 namespace mozhegova
 {
-  double sumArea();
-  void printCoorRect();
-  void scaleShapes();
-  double * getNum(char * tok, size_t length);
+  double sumArea(Shape ** shapes, size_t n);
+  void printCoorRect(Shape ** shapes, size_t n);
+  void scaleShapes(Shape ** shapes, size_t n, point_t p, double k);
+  void getNum(char * tok, size_t length, double * num);
+  void destroy(Shape ** shapes, size_t n);
 }
 
 #endif
