6d84a59887ce52d7ea6d0b4abb79c06ebbeea2de
mipxe <mariamozegova8726@gmail.com>
2024-12-12 23:37:30 +0300

    add output

--- mozhegova.maria/T1/main.cpp
+++ mozhegova.maria/T1/main.cpp
@@ -29,35 +29,68 @@ int main()
       continue;
     }
 
+    double * num = nullptr;
     char * tok = std::strtok(str, " ");
     bool isRectangle = std::strcmp(tok, "RECTANGLE") == 0;
     bool isScale = std::strcmp(tok, "SCALE") == 0;
 
     if (isRectangle)
     {
-      double * num = mozhegova::getNum(tok, 4);
+      try
+      {
+        mozhegova::getNum(tok, 4, num);
+      }
+      catch (const std::exception& e)
+      {
+        std::cerr << "Out of memory\n";
+        delete[] str;
+        mozhegova::destroy(shapes, count);
+        return 1;
+      }
       mozhegova::point_t lowLef = {num[0], num[1]};
       mozhegova::point_t uppRig = {num[2], num[3]};
       if (lowLef.x >= uppRig.x || lowLef.y >= uppRig.y)
       {
-        flag = false;
+        flag = true;
       }
       mozhegova::Rectangle * rect = new mozhegova::Rectangle(lowLef, uppRig);
       shapes[count++] = rect;
     }
     else if (isScale)
     {
-      double * num = mozhegova::getNum(tok, 3);
-      scaleCenter = {num[0], num[1]};
-      scaleCoef = num[2];
-      if (scaleCoef <= 0)
+      try
       {
+        mozhegova::getNum(tok, 3, num);
+      }
+      catch (const std::exception& e)
+      {
+        std::cerr << "Out of memory\n";
         delete[] str;
-        std::cerr << "Incorrent scale\n";
+        mozhegova::destroy(shapes, count);
         return 1;
       }
+      scaleCenter = {num[0], num[1]};
+      scaleCoef = num[2];
+      delete[] str;
       break;
     }
     delete[] str;
+  }
+  if (scaleCoef <= 0)
+  {
+    std::cerr << "Incorrect scale\n";
+    mozhegova::destroy(shapes, count);
+    return 1;
+  }
+  std::cout.precision(1);
+  std::cout << mozhegova::sumArea(shapes, count);
+  mozhegova::printCoorRect(shapes, count);
+  mozhegova::scaleShapes(shapes, count, scaleCenter, scaleCoef);
+  std::cout << mozhegova::sumArea(shapes, count);
+  mozhegova::printCoorRect(shapes, count);
+  mozhegova::destroy(shapes, count);
+  if (flag)
+  {
+    std::cerr << "There is an incorrect shape\n";
   }
 }
