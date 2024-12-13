6a29c937f622bd1a8461dfbdb7b5219775a911af
mipxe <mariamozegova8726@gmail.com>
2024-12-13 19:17:36 +0300

    CG 1.5 1.4

--- mozhegova.maria/T1/main.cpp
+++ mozhegova.maria/T1/main.cpp
@@ -12,7 +12,7 @@ int main()
   bool flag = false;
   double scaleCoef = 0.0;
   mozhegova::point_t scaleCenter = {0.0, 0.0};
-  while (!std::cin.eof()) 
+  while (!std::cin.eof())
   {
     try
     {
